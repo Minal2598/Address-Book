736161d891e960cf0a1809bc602d38dcc1ff0bff
Minal <patilminalp@gmail.com>
2021-06-27 23:27:20 +0530

    Added:Add multiple person to AddressBook

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.addressbook;
+
+public class Main {
+	public static void main(String[] args) {
+		   AddressBook addressbook = new AddressBook();
+		  // Main main = new Main();
+		   System.out.println("Welcome to Address Book Program....!!!");
+		   addressbook .User();
+			
+		}
+
+}
